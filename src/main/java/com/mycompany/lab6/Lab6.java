1e9924a69d6a318b64c03902201ee415d8ed37c1
dat060603 <Laptop ca t@LAPTOP-QOLGC39A>
2024-03-18 20:21:39 +0700

    testt

--- src/main/java/com/mycompany/lab6/Lab6.java
+++ src/main/java/com/mycompany/lab6/Lab6.java
@@ -7,6 +7,7 @@ package com.mycompany.lab6;
 public class Lab6 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!?????????");
     }
+    
 }
